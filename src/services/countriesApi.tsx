b79a7b6e974196b1d71d12a9aa6fd06c672e7373
unknown <m.nawrut34@gmail.com>
2023-11-19 13:12:19 +0100

    initialize project

---
+++ src/services/countriesApi.tsx
@@ -0,0 +1,4 @@
+// entire world: https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,subregion,borders,tld,currencies,languages
+// search country neighbors by code cca2 ccn3 : https://restcountries.com/v3.1/alpha/{code}
+// search by region: https://restcountries.com/v3.1/region/{region}?fields=name,flags,population,region,capital,subregion,borders,tld,currencies,languages
+//
